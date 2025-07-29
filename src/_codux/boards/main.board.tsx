dad2ba88f4512d2299adfe60f44c457aa23f2fc7
Kieran Pavy <kieran.pavy@proton.me>
2025-07-30 03:14:34 +1000

    Layed out screen 2 and 3

--- src/_codux/boards/main.board.tsx
+++ src/_codux/boards/main.board.tsx
@@ -1,6 +1,7 @@
 import "./main.board.css";
 import React from "react";
 import { createBoard } from "@wixc3/react-board";
+import BadgeColorSvg from "../../../assets/Media/badge-color.svg";
 
 export default createBoard({
   name: "Main",
@@ -25,7 +26,129 @@ export default createBoard({
           />
         </div>
       </div>
-      <div className="Scrollpoint"></div>
+      <div className="Scrollpoint">
+        <div className="MainBoard_div9">
+          <h2 className="MainBoard_header2">Stygian Descent</h2>
+        </div>
+        <div className="MainBoard_div8">
+          <iframe
+            src="https://www.youtube-nocookie.com/embed/qX_MJ94vBt0"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen="true"
+            class="card-embed"
+            className="MainBoard_iframe1"
+          ></iframe>
+          <div className="MainBoard_div10">
+            <p className="MainBoard_p2">
+              Single Player Roguelike Platformer with melee focused combat and
+              dozens of items 
+            </p>
+            <p className="MainBoard_p2">
+              I was the lead programmer on this project and created the
+              frameworks for items, enemy AI and level generation for the game
+              designers to use.
+            </p>
+          </div>
+        </div>
+        <div className="MainBoard_div11">
+          <div className="MainBoard_div12">
+            <p className="MainBoard_p3">
+              Roguelike - 3D - Side Scroller - Platformer 
+            </p>
+            <p className="MainBoard_p3">Role : Lead Programmer</p>
+          </div>
+          <div className="MainBoard_div13">
+            <button className="MainBoard_button2">
+              <img src={BadgeColorSvg} className="MainBoard_img4" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="Scrollpoint">
+        <div className="MainBoard_grid">
+          <div className="MainBoard_div15">
+            <img
+              src="https://static.wixstatic.com/media/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg/v1/fill/w_600,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg"
+              alt=""
+              className="MainBoard_img5"
+            />
+            <p className="MainBoard_p4">BnB Arcade Clone</p>
+            <p className="MainBoard_p5">
+              Created in a team of 2, we aimed to recreate the korean arcade
+              game BnB Arcade using Unity.  Given the small team, I both worked
+              on all aspects of the game from art to programming and animation.
+            </p>
+          </div>
+          <div className="MainBoard_div15">
+            <img
+              src="https://static.wixstatic.com/media/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg/v1/fill/w_600,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg"
+              alt=""
+              className="MainBoard_img5"
+            />
+            <p className="MainBoard_p4">Pacts of 10</p>
+            <p className="MainBoard_p5">
+              Created in a team of 2, we aimed to recreate the korean arcade
+              game BnB Arcade using Unity.  Given the small team, I both worked
+              on all aspects of the game from art to programming and animation.
+            </p>
+          </div>
+          <div className="MainBoard_div15">
+            <img
+              src="https://static.wixstatic.com/media/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg/v1/fill/w_600,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg"
+              alt=""
+              className="MainBoard_img5"
+            />
+            <p className="MainBoard_p4">Witch Tales</p>
+            <p className="MainBoard_p5">
+              Created in a team of 2, we aimed to recreate the korean arcade
+              game BnB Arcade using Unity.  Given the small team, I both worked
+              on all aspects of the game from art to programming and animation.
+            </p>
+          </div>
+          <div className="MainBoard_div15">
+            <img
+              src="https://static.wixstatic.com/media/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg/v1/fill/w_600,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg"
+              alt=""
+              className="MainBoard_img5"
+            />
+            <p className="MainBoard_p4">BnB Arcade Clone</p>
+            <p className="MainBoard_p5">
+              Created in a team of 2, we aimed to recreate the korean arcade
+              game BnB Arcade using Unity.  Given the small team, I both worked
+              on all aspects of the game from art to programming and animation.
+            </p>
+          </div>
+          <div className="MainBoard_div15">
+            <img
+              src="https://static.wixstatic.com/media/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg/v1/fill/w_600,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg"
+              alt=""
+              className="MainBoard_img5"
+            />
+            <p className="MainBoard_p4">Cult of Azathoth</p>
+            <p className="MainBoard_p5">
+              Created in a team of 2, we aimed to recreate the korean arcade
+              game BnB Arcade using Unity.  Given the small team, I both worked
+              on all aspects of the game from art to programming and animation.
+            </p>
+          </div>
+          <div className="MainBoard_div15">
+            <img
+              src="https://static.wixstatic.com/media/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg/v1/fill/w_600,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg"
+              alt=""
+              className="MainBoard_img5"
+            />
+            <p className="MainBoard_p4">BnB Arcade Clone</p>
+            <p className="MainBoard_p5">
+              Created in a team of 2, we aimed to recreate the korean arcade
+              game BnB Arcade using Unity.  Given the small team, I both worked
+              on all aspects of the game from art to programming and animation.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   ),
   environmentProps: {
